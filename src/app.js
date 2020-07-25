f6cdf7626f56c116e1bfd7e2c5582a611e0c6642
unknown <chandankumarshani98@gmail.com>
2020-07-25 12:31:44 +0545

    remove dummy expnse data

--- src/app.js
+++ src/app.js
@@ -11,14 +11,6 @@ import './styles/styles.scss';
 
 const store = configureStore();                                 //storing the return value of store from configureStore to store variable
 
-store.dispatch(addExpense({ description: 'water bill', amount: 5000}));
-store.dispatch(addExpense({ description: 'gas bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 10500 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>                        {/*Provider allows the component to access the store */}
         <AppRouter/>
